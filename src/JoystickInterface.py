a8bd7e65f29a74eefc380c51e02db430faa94880
ctingley <connoson@gmail.com>
2020-06-02 11:38:15 -0700

    first

--- src/JoystickInterface.py
+++ src/JoystickInterface.py
@@ -1,6 +1,7 @@
 import UDPComms
 import numpy as np
 import time
+import xbox
 from src.State import BehaviorState, State
 from src.Command import Command
 from src.Utilities import deadband, clipped_first_order_filter
@@ -8,34 +9,33 @@ from src.Utilities import deadband, clipped_first_order_filter
 
 class JoystickInterface:
     def __init__(
-        self, config, udp_port=8830, udp_publisher_port = 8840,
+        self, config,
     ):
         self.config = config
         self.previous_gait_toggle = 0
         self.previous_state = BehaviorState.REST
         self.previous_hop_toggle = 0
         self.previous_activate_toggle = 0
+        self.joy = xbox.Joystick()
 
-        self.message_rate = 50
-        self.udp_handle = UDPComms.Subscriber(udp_port, timeout=0.3)
-        self.udp_publisher = UDPComms.Publisher(udp_publisher_port)
+
 
 
     def get_command(self, state, do_print=False):
-        try:
+        if joy.connected():
             msg = self.udp_handle.get()
             command = Command()
             
             ####### Handle discrete commands ########
             # Check if requesting a state transition to trotting, or from trotting to resting
-            gait_toggle = msg["R1"]
+            gait_toggle = joy.rightBumper()
             command.trot_event = (gait_toggle == 1 and self.previous_gait_toggle == 0)
 
             # Check if requesting a state transition to hopping, from trotting or resting
-            hop_toggle = msg["x"]
+            hop_toggle = joy.X()
             command.hop_event = (hop_toggle == 1 and self.previous_hop_toggle == 0)            
             
-            activate_toggle = msg["L1"]
+            activate_toggle = joy.leftBumper()
             command.activate_event = (activate_toggle == 1 and self.previous_activate_toggle == 0)
 
             # Update previous values for toggles and state
@@ -44,15 +44,14 @@ class JoystickInterface:
             self.previous_activate_toggle = activate_toggle
 
             ####### Handle continuous commands ########
-            x_vel = msg["ly"] * self.config.max_x_velocity
-            y_vel = msg["lx"] * -self.config.max_y_velocity
+            x_vel = joy.leftX() * self.config.max_x_velocity
+            y_vel = joy.leftY() * -self.config.max_y_velocity
             command.horizontal_velocity = np.array([x_vel, y_vel])
-            command.yaw_rate = msg["rx"] * -self.config.max_yaw_rate
+            command.yaw_rate = joy.rightX() * -self.config.max_yaw_rate
 
-            message_rate = msg["message_rate"]
-            message_dt = 1.0 / message_rate
+            message_dt = joy.refreshDelay
 
-            pitch = msg["ry"] * self.config.max_pitch
+            pitch = joy.rightY() * self.config.max_pitch
             deadbanded_pitch = deadband(
                 pitch, self.config.pitch_deadband
             )
@@ -64,20 +63,18 @@ class JoystickInterface:
             )
             command.pitch = state.pitch + message_dt * pitch_rate
 
-            height_movement = msg["dpady"]
+            height_movement = joy.dpadUp() - joy.dpadDown()
             command.height = state.height - message_dt * self.config.z_speed * height_movement
             
-            roll_movement = - msg["dpadx"]
+            roll_movement = joy.dpadRight() - joy.dpadLeft()
             command.roll = state.roll + message_dt * self.config.roll_speed * roll_movement
 
             return command
 
-        except UDPComms.timeout:
-            if do_print:
-                print("UDP Timed out")
+        else :
+            print("Xbox Controller Not Connected")
             return Command()
 
 
     def set_color(self, color):
-        joystick_msg = {"ps4_color": color}
-        self.udp_publisher.send(joystick_msg)
+        print("Color change does not work with Xbox controller")
